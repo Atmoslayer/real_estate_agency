2ea7ab8eb4d4b5717d41c65301acebb0fc774083
Atmoslayer <ddsomdim@gmail.com>
2022-11-28 21:48:22 +0300

    Fix complaint model related names

--- property/models.py
+++ property/models.py
@@ -67,13 +67,13 @@ class Complaint(models.Model):
         User,
         on_delete=models.CASCADE,
         verbose_name='Кто жаловался',
-        related_name='who_complained'
+        related_name='complaints'
     )
     flat = models.ForeignKey(
         Flat,
         on_delete=models.CASCADE,
         verbose_name='Квартира, на которую жаловались',
-        related_name='complained_flat'
+        related_name='complaints'
     )
     text = models.TextField('Текст жалобы', default='')
 
